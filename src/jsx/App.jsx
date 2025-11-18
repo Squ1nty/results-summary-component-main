1fc14d8b65a0e2242154874847e74e4c4368aa47
Vincent Dao <vincentdao288@gmail.com>
2025-11-18 14:28:03 +1100

    Initial commit

---
+++ src/jsx/App.jsx
@@ -0,0 +1,12 @@
+import '../css/App.css'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div className="h-svh">
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/jsx/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+  return(
+    <div className="text-base text-center">
+      Challenge by <a style={{ color: 'hsl(228, 45%, 44%)' }} href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a style={{ color: 'hsl(228, 45%, 44%)' }} href="https://www.frontendmentor.io/profile/Squ1nty">Vincent Dao</a>.
+    </div>
+  );
+}
+
+export default Footer
